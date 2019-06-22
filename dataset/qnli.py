300c8a2cf2f17876de8af9cbb07106c090c861fe
zetong <zqi38@wisc.edu>
2019-06-22 14:10:41 -0500

    modified qnli.py, added mag_model download script

--- dataset/qnli.py
+++ dataset/qnli.py
@@ -3,15 +3,18 @@ import urllib.request
 import zipfile
 
 DIR = os.path.dirname(os.path.abspath(__file__))
-ZIP_FILE = os.path.join(DIR, "qnli.zip")
 DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FQNLIv2.zip?alt=media&token=6fdcf570-0fc5-4631-8456-9505272d1601"
-TRAIN = os.path.join(DIR, "QNLI", "train.tsv")
-VAL = os.path.join(DIR, "QNLI", "dev.tsv")
-TEST = os.path.join(DIR, "QNLI", "test.tsv")
+RAW = os.path.join(DIR, "raw")
+ZIP_FILE = os.path.join(RAW, "qnli.zip")
+TRAIN = os.path.join(RAW, "QNLI", "train.tsv")
+VAL = os.path.join(RAW, "QNLI", "dev.tsv")
+TEST = os.path.join(RAW, "QNLI", "test.tsv")
 
 if __name__ == '__main__':
 	if not os.path.exists(ZIP_FILE):
+		print("downloading QNLI dataset")
+		os.mkdir(RAW)
 		urllib.request.urlretrieve(DOWNLOAD_URL, ZIP_FILE)
 		zip_ref = zipfile.ZipFile(ZIP_FILE, 'r')
-		zip_ref.extractall(DIR)
+		zip_ref.extractall(RAW)
 		zip_ref.close()
